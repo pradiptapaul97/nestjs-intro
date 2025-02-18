4a057cc9bd7eee2ecbaecb18012e0a12be12509d
Pradipta Paul <pradipta.paul@webskitters.com>
2025-02-19 01:31:47 +0530

    extend swegger setup for post

--- src/posts/dtos/create-post.dto.ts
+++ src/posts/dtos/create-post.dto.ts
@@ -16,6 +16,7 @@ import { PostType } from '../enums/postType.enum';
 import { PostStatus } from '../enums/postStatus.enum';
 // import { PostMetaOptions } from '../enums/postMetaOptions.enum';
 import { CreatePostMetaOptionDto } from './create-post-meta-option.dto';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 /**
  * Structure of request body to create a new post
@@ -33,15 +34,28 @@ import { CreatePostMetaOptionDto } from './create-post-meta-option.dto';
  */
 
 export class CreatePostDto {
+  @ApiProperty({
+    example:"This is a title",
+    description:"This is the title for blog post"
+  })
   @IsString()
   @MinLength(4)
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty({
+    enum: PostType,
+    description:"This is the post type for blog post.Possible values post, page, story, series"
+  })
   @IsEnum(PostType)
   @IsNotEmpty()
   postType: PostType;
 
+  @ApiProperty({
+    enum: PostType,
+    description:"This is the slug for blog post",
+    example: "my-blog-post"
+  })
   @IsString()
   @IsNotEmpty()
   @Matches(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, {
@@ -49,37 +63,82 @@ export class CreatePostDto {
   })
   slug: string;
 
+  @ApiProperty({
+    enum: PostStatus,
+    description:"This is the status for blog post.Possible values draft, scheduled, review, published",
+    example: "my-blog-post"
+  })
   @IsEnum(PostStatus)
   @IsNotEmpty()
   status: PostStatus;
 
+  @ApiPropertyOptional({
+    example:"This is a content",
+    description:"This is the content for blog post"
+  })
   @IsOptional()
   @IsString()
   content?: string;
 
+  @ApiPropertyOptional({
+    example:"{\"key\": \"value\"}",
+    description:"Serialize your JSON object."
+  })
   @IsOptional()
   @IsJSON()
   schema?: string;
 
+  @ApiPropertyOptional({
+    example:"https://images.pexels.com/photos/2990603/pexels-photo-2990603.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    description:"This is the featured image for blog post"
+  })
   @IsOptional()
   @IsUrl()
   featuredImageUrl?: string;
 
   //@IsDate()
+  @ApiPropertyOptional({
+    example:"2025-02-17T12:00:00Z",
+    description:"This is the date for blog post"
+  })
   @IsISO8601()
   @IsOptional()
   // @Type(() => Date)
   publishOn?: Date;
 
+  @ApiPropertyOptional({
+    example:['first','second'],
+    description:"This is the tag for blog post which is array of string"
+  })
   @IsOptional()
   @IsArray()
   @IsString({ each: true })
   @MinLength(3, { each: true })
   tags?: string[];
 
+  //optional but when given the key value ar proper formated
+  @ApiPropertyOptional({
+    type: 'array',
+    required: false,
+    items: {
+      type:'object',
+      properties:{
+        key:{
+          type: 'string',
+          description: "The key can be any string",
+          example:'SidebarEnable'
+        },
+        value:{
+          type: 'any',
+          description: "The value can be any",
+          example:true
+        }
+      }
+    }
+  })
   @IsOptional()
   @IsArray()
   @ValidateNested({ each: true })
   @Type(() => CreatePostMetaOptionDto)
-  metaOptions: CreatePostMetaOptionDto[];
+  metaOptions?: CreatePostMetaOptionDto[];
 }
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -22,9 +22,6 @@ export class PostsController {
   }
 
   @Post()
-  @ApiBody({
-    //
-  })
   /**
    * createPost
    */
