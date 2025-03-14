b4863709768fbe4a7614c6d8bc751b0cf9ace63c
Pradipta Paul <pradipta.paul@webskitters.com>
2025-03-14 21:49:27 +0530

    set env

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -15,10 +15,18 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
+  // it('/ (GET)', () => {
+  //   return request(app.getHttpServer())
+  //     .get('/')
+  //     .expect(200)
+  //     .expect('Hello World!');
+  // });
+
   it('/ (GET)', () => {
+    console.log(process.env.NODE_ENV);
+
     return request(app.getHttpServer())
       .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .expect(404);
   });
 });
